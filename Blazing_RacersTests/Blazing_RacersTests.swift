9e134010638231f017f1857f87ace9351c759ee3
nmalin-jones <student@bluc02mg3uefh51.local>
2016-03-29 11:23:11 -0500

    Initial Commit

---
+++ Blazing_RacersTests/Blazing_RacersTests.swift
@@ -0,0 +1,36 @@
+//
+//  Blazing_RacersTests.swift
+//  Blazing_RacersTests
+//
+//  Created by nmalin-jones on 3/29/16.
+//  Copyright © 2016 nmalin-jones. All rights reserved.
+//
+
+import XCTest
+@testable import Blazing_Racers
+
+class Blazing_RacersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
