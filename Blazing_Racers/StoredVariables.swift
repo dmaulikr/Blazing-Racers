824b4fde93c00d6610794cdb0592405656d0105a
cstark <student@cstark.local>
2016-04-22 11:29:46 -0500

    It's Claymore

--- Blazing_Racers/StoredVariables.swift
+++ Blazing_Racers/StoredVariables.swift
@@ -1,13 +1,10 @@
-//
-//  StoredVariables.swift
-//  Blazing_Racers
-//
-//  Created by nmalin-jones on 4/14/16.
-//  Copyright © 2016 nmalin-jones. All rights reserved.
-//
+
 
 import UIKit
 
 class StoredVariables: NSObject {
-//meme
+//Amount of people : Boolean
+//Bluetooth : boolean
+//car: String
+    
 }
