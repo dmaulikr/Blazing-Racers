82f672f54f416c1d3d27ffabb2573b413016acac
cstark <student@cstark.local>
2016-04-26 11:23:36 -0500

    lol

--- Blazing_Racers/MainView.swift
+++ Blazing_Racers/MainView.swift
@@ -3,7 +3,7 @@
 import UIKit
 
 class MainViewViewController: UIViewController {
-
+var variables = StoredVariables()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -13,10 +13,34 @@ class MainViewViewController: UIViewController {
         super.didReceiveMemoryWarning()
 
     }
-    @IBAction func singlePlayerButton(sender: AnyObject)
+    
+    @IBAction func onePlayerButtonTapped(sender: UIButton)
     {
-       
+
     }
     
+    @IBAction func splitScreenButtonTapped(sender: UIButton)
+    {
 
+    }
+    
+    @IBAction func multiPlayerButtonTapped(sender: AnyObject)
+    {
+
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let singleVC = segue.destinationViewController as! SinglePlayerView
+        let multiVC = segue.destinationViewController as! MultiplayerView
+        let splitVC = segue.destinationViewController as! SplitScreenView
+        if segue.identifier == "onePlayerSegue" {
+            variables.singlePlayer = true
+            print(variables.singlePlayer.boolValue)
+        } else if segue.identifier == "splitScreenSegue" {
+            variables.splitScreen = true
+            print(variables.splitScreen.boolValue)
+        } else if segue.identifier == "multiPlayerSegue" {
+            variables.Bluetooth = true
+            print(variables.Bluetooth.boolValue)
+        }
+    }
 }
