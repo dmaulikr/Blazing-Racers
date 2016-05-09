d587dc42989b7292ff1f071e8f2750be1beb3d8f
JoseRamonSoto <familysoto1725@icloud.com>
2016-05-09 11:41:46 -0500

    Bluetooth files, no rotation

---
+++ UIView+HostViewController.m
@@ -0,0 +1,13 @@
+//
+//  UIView+HostViewController.m
+//  Blazing_Racers
+//
+//  Created by Jose Ramon Soto on 5/9/16.
+//  Copyright © 2016 nmalin-jones. All rights reserved.
+//
+
+#import "UIView+HostViewController.h"
+
+@implementation UIView (HostViewController)
+
+@end
